782e8ea2bb4a769c2dec94c5227a69e3106d263b
Brandonage <alvarobrandon@gmail.com>
2015-08-06 17:01:19 +0100

    Change some parameters for better visualisation
    - Change the size of some of the fonts and legends

--- ponpareAnalysis.R
+++ ponpareAnalysis.R
@@ -1,6 +1,8 @@
 ######## Analysis of the Ponpare Groupon Data #########
 
 library(data.table)
+library(plyr)
+library(scales)
 
 #### the purchase log of users buying coupons . It has a unique value that is purchaseID_hash
 cpdetailpath <- '/Users/alvarobrandon/GitHub/AuxFiles/Ponpare Groupon/coupon_detail_train.csv' 
@@ -79,22 +81,49 @@ m + geom_histogram(bindwidth = 5,breaks=seq(15,80,by=5),aes(fill=..count..)) +
 
 m <- ggplot(purchases, aes(x=reorder(GENRE_NAME,GENRE_NAME,function(x)-length(x))))
 m + geom_freqpoly(aes(color=AGE,group=AGE)) + theme_minimal() + 
-  labs(x='Coupon Type',title='Coupon types for each Age category')
+  labs(x='Coupon Type',title='Coupon types for each Age category') +
+  theme(text = element_text(size=16),axis.text.x = element_text(angle=90))
 
 ### What are the distribution of Sexes inside each category?
 
 m <- ggplot(purchases, aes(x=reorder(GENRE_NAME,GENRE_NAME,function(x)-length(x))))
 m + geom_bar(aes(fill=SEX_ID)) + theme_minimal() +
-  labs(x='Coupon Type',title='Sex and type of coupon')
+  labs(x='Coupon Type',title='Sex and type of coupon') +
+theme(text = element_text(size=16),axis.text.x = element_text(angle=90))
 
 
+#### BROWSING HISTORY   #####
+
+setkey(cpvisits,VIEW_COUPON_ID_hash)
+history <- cpvisits[cplist]
+## We change the long string depicting a hotel into a smaller one.
+history[GENRE_NAME=='Hotel and Japanese hotel',GENRE_NAME := 'Hotel']
+
+### We are going to plot the ratio of coupons purchased/browsed by coupon type
+
+### It is difficult to do transformations of the data through ggplot, so we are 
+### going to make that separately by using grouping by with data.table
+ans <- history[,.(.N),by=.(GENRE_NAME,PURCHASE_FLG)]
+### We divide the data frame by GENRE_NAME and we operate with each piece. In this case we
+### transform the data frame adding a new percent column
+ans = ddply(ans,.(GENRE_NAME),transform,percent=N/sum(N)*100)
+### Same here but with the position
+ans = ddply(ans,.(GENRE_NAME),transform, pos=cumsum(N) - 0.5*N)
+### Another new column with the label for the barchart
+ans$label = paste0(sprintf("%.0f", ans$percent), "%")
+### We take out the row that has NA's values 
+ans <- ans[-11,]
+### We add an offset to the ones that have been purchased. This is so because in some bars 
+### they are too close together so we push them forward in the chart
+ans[ans$PURCHASE_FLG==1,]$pos <- ans[ans$PURCHASE_FLG==1,]$pos + 25000
 
 
-
-
-
-
-
+### We do the plot with the different parameters
+ggplot(ans,aes(x=reorder(GENRE_NAME,N) , y = N, fill=factor(PURCHASE_FLG))) + 
+  geom_bar(stat="identity") + geom_text(aes(y=pos,label=label)) +
+  scale_y_continuous(name="Number of Visits",labels=comma) + labs(x='Coupon Type')  + coord_flip() +
+  scale_fill_discrete(name="Purchase Flag",labels=c("Not Purchased","Purchased")) + theme_minimal() +
+  theme(text = element_text(size=16))
 
 
 
